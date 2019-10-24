213585e9ec35a2ea9a521f39ba5c4a7e3ab847a5
Gleb Potapov <48017740+senketsukamui@users.noreply.github.com>
2019-10-24 17:27:34 +0300

    Update index.js

--- 5/index.js
+++ 5/index.js
@@ -13,7 +13,8 @@ const init = (() => {
 
     document.querySelector(".tic-tac-toe").onclick = (event) => {
         if (event.target.value == "") {
-            turn ? event.target.value = "x" : event.target.value = "o";
+            turn ? event.target.value = "X" : event.target.value = "O";
+            event.target.disabled = "disabled"; 
             turn = !turn;
 
         }
@@ -24,74 +25,72 @@ const init = (() => {
 
 const boardCheck = (boardState, symbol) => {
     if (boardState[0].value === symbol && boardState[1].value === symbol && boardState[2].value === symbol) {
-        boardState[0].style.backgroundColor = "green";
-        boardState[1].style.backgroundColor = "green";
-        boardState[2].style.backgroundColor = "green";
+        boardState[0].style.backgroundColor = "gray";
+        boardState[1].style.backgroundColor = "gray";
+        boardState[2].style.backgroundColor = "gray";
         reloadGame(symbol)
 
     }
 
     if (boardState[3].value === symbol && boardState[4].value === symbol && boardState[5].value === symbol) {
-        boardState[3].style.backgroundColor = "green";
-        boardState[4].style.backgroundColor = "green";
-        boardState[5].style.backgroundColor = "green";
+        boardState[3].style.backgroundColor = "gray";
+        boardState[4].style.backgroundColor = "gray";
+        boardState[5].style.backgroundColor = "gray";
         reloadGame(symbol)
 
     }
     if (boardState[6].value === symbol && boardState[7].value === symbol && boardState[8].value === symbol) {
-        boardState[6].style.backgroundColor = "green";
-        boardState[7].style.backgroundColor = "green";
-        boardState[8].style.backgroundColor = "green";
+        boardState[6].style.backgroundColor = "gray";
+        boardState[7].style.backgroundColor = "gray";
+        boardState[8].style.backgroundColor = "gray";
         reloadGame(symbol)
     }
 
     if (boardState[0].value === symbol && boardState[3].value === symbol && boardState[6].value === symbol) {
-        boardState[0].style.backgroundColor = "green";
-        boardState[3].style.backgroundColor = "green";
-        boardState[6].style.backgroundColor = "green";
+        boardState[0].style.backgroundColor = "gray";
+        boardState[3].style.backgroundColor = "gray";
+        boardState[6].style.backgroundColor = "gray";
         reloadGame(symbol)
     }
 
     if (boardState[1].value === symbol && boardState[4].value === symbol && boardState[7].value === symbol) {
-        boardState[1].style.backgroundColor = "green";
-        boardState[4].style.backgroundColor = "green";
-        boardState[7].style.backgroundColor = "green";
+        boardState[1].style.backgroundColor = "gray";
+        boardState[4].style.backgroundColor = "gray";
+        boardState[7].style.backgroundColor = "gray";
         reloadGame(symbol)
     }
 
     if (boardState[2].value === symbol && boardState[5].value === symbol && boardState[8].value === symbol) {
-        boardState[2].style.backgroundColor = "green";
-        boardState[5].style.backgroundColor = "green";
-        boardState[8].style.backgroundColor = "green";
+        boardState[2].style.backgroundColor = "gray";
+        boardState[5].style.backgroundColor = "gray";
+        boardState[8].style.backgroundColor = "gray";
         reloadGame(symbol)
     }
     if (boardState[0].value === symbol && boardState[4].value === symbol && boardState[8].value === symbol) {
-        boardState[0].style.backgroundColor = "green";
-        boardState[4].style.backgroundColor = "green";
-        boardState[8].style.backgroundColor = "green";
+        boardState[0].style.backgroundColor = "gray";
+        boardState[4].style.backgroundColor = "gray";
+        boardState[8].style.backgroundColor = "gray";
         reloadGame(symbol)
     }
     if (boardState[2].value === symbol && boardState[4].value === symbol && boardState[6].value === symbol) {
-        boardState[2].style.backgroundColor = "green";
-        boardState[4].style.backgroundColor = "green";
-        boardState[6].style.backgroundColor = "green";
+        boardState[2].style.backgroundColor = "gray";
+        boardState[4].style.backgroundColor = "gray";
+        boardState[6].style.backgroundColor = "gray";
         reloadGame(symbol)
     }
 }
-
 const reloadGame = (symbol) => {
     isEnded = true;
-    symbol == "x" ? setTimeout(() => (alert("X won!"), window.location.reload()), 30) : setTimeout(() => (alert("O won!"), window.location.reload()), 30);
+    symbol == "X" ? setTimeout(() => (alert("X won!"), window.location.reload()), 30) : setTimeout(() => (alert("O won!"), window.location.reload()), 30);
 }
 
 const winnerCheck = () => {
     boardValues = document.getElementsByClassName('tic-tac-toe-button');
-    console.log(boardValues)
-    boardCheck(boardValues, "x");
-    boardCheck(boardValues, "o");
+    boardCheck(boardValues, "X");
+    boardCheck(boardValues, "O");
     checkedPositions++;
     if (checkedPositions == 9 && !isEnded) {
-        alert("Draw!");
+        setTimeout(() => alert("Draw!"));
         checkedPositions = 0;
         isEnded = false;
         turn = true;
